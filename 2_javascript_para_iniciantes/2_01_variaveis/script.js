70790d6c3fc698a790bb81882911005f8e793e3e
Artur Ribeiro <profissional.arturrc@gmail.com>
2023-10-23 17:47:52 -0300

    aula_variaveis

---
+++ 2_javascript_para_iniciantes/2_01_variaveis/script.js
@@ -0,0 +1,33 @@
+var nome = 'Artur';
+let nomeLet= "Artur com Let";
+const nomeConst = "Artur com const";
+
+
+//variáveis encadeadas.
+//chamei um let só e registrei 2 variáveis e isso funciona
+let cidade = "Mogi das Cruzes", Bairro = "Vila Jundiaí";
+
+
+
+//Exercicios
+
+// Declarar uma variável com o seu nome
+var meuNome = "Artur";
+
+// Declarar uma variável com a sua idade
+var minhaIdade = "33";
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+
+var pratoPreferido;
+
+// Atribuir valor a sua comida favorita
+pratoPreferido = "São muitos, não tenho preferido.";
+
+// Declarar 5 variáveis diferentes sem valores
+var var1;
+var var2;
+var var3;
+var var4;
+var var5;
